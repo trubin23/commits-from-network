87deb59e35c366daa99a22189e9ebef1ae4ec875
Andrey Trubin <trubin23@gmail.com>
2018-01-08 23:59:20 +0500

    added DetailActivity class

---
+++ app/src/main/java/com/example/trubin23/commitsfromnetwork/presentation/commits/showdetails/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.trubin23.commitsfromnetwork.presentation.commits.showdetails;
+
+import com.example.trubin23.commitsfromnetwork.presentation.common.BaseActivity;
+
+/**
+ * Created by Andrey on 08.01.2018.
+ */
+
+public class DetailActivity extends BaseActivity {
+}
