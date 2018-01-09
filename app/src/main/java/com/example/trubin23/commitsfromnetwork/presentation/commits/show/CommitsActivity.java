dea2e81a8ccbd6343a418fd756d725288e4aa0dc
Andrey Trubin <trubin23@gmail.com>
2018-01-09 23:59:42 +0500

    added AlertDialog

--- app/src/main/java/com/example/trubin23/commitsfromnetwork/presentation/commits/show/CommitsActivity.java
+++ app/src/main/java/com/example/trubin23/commitsfromnetwork/presentation/commits/show/CommitsActivity.java
@@ -2,7 +2,9 @@ package com.example.trubin23.commitsfromnetwork.presentation.commits.show;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,16 +14,12 @@ import com.example.trubin23.commitsfromnetwork.presentation.common.BaseActivity;
 
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CommitsActivity extends BaseActivity implements CommitsContract.View {
 
     private static final String TAG = CommitsActivity.class.getSimpleName();
-
-    @BindView(R.id.et_repo_name)
-    EditText mRepoName;
 
     private CommitsPresenter mPresenter;
 
@@ -41,8 +39,23 @@ public class CommitsActivity extends BaseActivity implements CommitsContract.Vie
 
     @OnClick(R.id.btn_load_commits)
     public void onClickLoadCommits(View v) {
-        String repoName = mRepoName.getText().toString();
-        mPresenter.loadCommits(repoName);
+//        String repoName = mRepoName.getText().toString();
+//        mPresenter.loadCommits(repoName);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View dialogRepoName = layoutInflater.inflate(R.layout.dialog_repo_name, null);
+
+        final EditText repoName = (EditText) dialogRepoName.findViewById(R.id.et_repo_name);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogRepoName);
+        builder.setTitle(R.string.label_repo_name);
+
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
