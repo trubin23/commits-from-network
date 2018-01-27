2a0da37e1de58b0f627cc465e59168a9e21ccb52
Andrey Trubin <trubin23@gmail.com>
2018-01-27 23:49:26 +0500

    process adding Fragment for CommitDerail

---
+++ app/src/main/java/com/example/trubin23/commitsfromnetwork/commitdetail/CommitDetailFragment.java
@@ -0,0 +1,10 @@
+package com.example.trubin23.commitsfromnetwork.commitdetail;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Andrey on 27.01.2018.
+ */
+
+public class CommitDetailFragment extends Fragment {
+}
